a0b8ed3f2f5d6945be7c957855eeef05b895ac87
Irina444444 <staat4444@gmail.com>
2022-10-05 11:44:52 +0300

    Added code program

---
+++ Task/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+      string [] text;
+
+      text = Console.ReadLine().Split(' ', ',', ';', '.');
+      for (int i = 0; i < text.Length; i++)
+      {
+        if (text[i].Length < 3)
+        {
+            Console.WriteLine(text[i]);
+        }
+      
+      }
+        Console.ReadKey();
